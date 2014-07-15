9c6c0bcb3dcbf8ea93db18aa8e14f9f96fe4f1de
Bastiaan Quast <bquast@gmail.com>
2014-07-15 08:27:45 +0200

    possibly remove attach

--- R/wwz.R
+++ R/wwz.R
@@ -16,7 +16,7 @@ wwz <- function( x, return.totals=FALSE ) {
   
   # Part 2: Decomposing Export into VA( 16 items )
   # defining ALL to contain all decomposed results
-  ALL  <- array( 0,dim=c( GN, G, 19 ) )
+  ALL  <- array( 0,dim=c( x$GN, x$G, 19 ) )
   
   # the order of 16 items and exp, expint, expfd
   decomp19 <- c(  "DVA_FIN",
@@ -41,22 +41,25 @@ wwz <- function( x, return.totals=FALSE ) {
   )
   
   
-  ALL[ ,,17 ] <- ESR
-  ALL[ ,,18 ] <- Eint
-  ALL[ ,,19 ] <- Efd
+  ALL[ ,,17 ] <- x$ESR
+  ALL[ ,,18 ] <- x$Eint
+  ALL[ ,,19 ] <- x$Efd
   
   
   ### find out why this returns an error
   # rm( Eint, Efd )
+  x$Eint <- NULL
+  x$Efd <- NULL
   gc()
   
   # Part 2-1 == H10-(1): DVA_FIN=[ VsBss#Ysr ]
   # OK
-  for ( r in 1: G ) {
-    z1 <- array(Ym[,r ], dim=c( GN, GN))
-    ALL[ ,r,1 ]  <- colSums( diag(Vc)%*%Bd*t(z1) )
+  for ( r in 1: x$G ) {
+    z1 <- array(x$Ym[,r ], dim=c( x$GN, x$GN))
+    ALL[ ,r,1 ]  <- colSums( diag(x$Vc)%*%x$Bd*t(z1) )
   }
-  rm( z1 )
+  # rm( z1 )
+  z1 <- NULL
   gc()
   
   # View( ALL[ ,,1 ] )   # DVA_FIN
@@ -64,162 +67,198 @@ wwz <- function( x, return.totals=FALSE ) {
   # Part 2-2: H10-(2): DVA_INT
   
   # OK
-  VsLss <- diag( Vc )%*%L
-  z1 <- Am%*%Bd%*%Yd
-  for ( r in 1: G ) {
+  VsLss <- diag( x$Vc ) %*% x$L
+  z1 <- x$Am %*% x$Bd %*% x$Yd
+  for ( r in 1: x$G ) {
     # r=2
     ALL[ ,r,2 ] <- colSums(VsLss)*t(z1[ ,r ])
   }
   
-  rm( z1 )
+  # rm( z1 )
+  z1 <- NULL
   gc()
   
   # View( ALL[ ,,2 ] )   #  DVA_INT
   
   # Part 2-3: H10-(3): DVA_INTrexI1
   # OK
-  z1 <- array(rowSums( Yd), dim=c( GN,GN))
-  for ( tt in 1: G ) {
-    m=1+( tt-1 )*N; n=N+(tt-1)*N
+  z1 <- array(rowSums( x$Yd), dim=c( x$GN, x$GN))
+  for ( tt in 1:x$G ) {
+    m=1+( tt-1 )*x$N
+    n=x$N+(tt-1)*x$N
     z1[ m:n,m:n ] <- 0
   }
-  z2 <- Bm%*%z1
+  z2 <- x$Bm %*% z1
+  
   for ( tt in 1: G ) {
-    m=1+( tt-1 )*N; n=N+(tt-1)*N
+    m=1+( tt-1 )*N
+    n=x$N+(tt-1)*x$N
     z2[ m:n,m:n ] <- 0
   }
-  z3 <- Am*t( z2 )
-  for ( r in 1: G ) {
-    m=1+( r-1 )*N
-    
-    n=N+(r-1)*N
+  
+  z3 <- x$Am * t( z2 )
+  
+  for ( r in 1:x$G ) {
+    m <- 1 + (r-1) * x$N
+    n <-  x$N + (r-1) * x$N
     ALL[ ,r,3 ] <- colSums(VsLss)*(rowSums(z3[ ,m:n ]))
   }
-  rm( z1,z2,z3 )
+  # rm( z1,z2,z3 )
+  x$z1 <- NULL
+  x$z2 <- NULL
+  x$z3 <- NULL
   gc()
   # View( ALL[ ,,3 ] )  #  DVA_INTrexI1
   
   # Part 2-4: H10-(4): DVA_INTrexF
   # OK
-  z <- array( 0, dim=c( GN,GN ) )
-  z1 <- rowSums( Ym )
-  for ( tt in 1:G ){
-    m <- 1+( tt-1 )*N; n <-N+(tt-1)*N
-    z[,m:n] <- z1 - Ym[ ,tt ]
+  z <- array( 0, dim=c( x$GN, x$GN ) )
+  z1 <- rowSums( x$Ym )
+  for ( tt in 1:x$G ){
+    m <- 1 + (tt-1) * x$N
+    n <- x$N + (tt-1) * x$N
+    z[,m:n] <- z1 - x$Ym[ ,tt ]
     z[ m:n,m:n ] <- 0
   }
   
-  z2 <- Am*t( Bd%*%z )
-  for ( r in 1: G ) {
-    m <- 1+( r-1 )*N; n <- N+(r-1)*N
-    ALL[ ,r,4 ] <- colSums(VsLss)*(rowSums(z2[ ,m:n ]))
+  z2 <- x$Am * t(x$Bd %*% z)
+  for ( r in 1:x$G ) {
+    m <- 1+( r-1 ) * x$N
+    n <- x$N + (r-1) * x$N
+    ALL[ ,r,4 ] <- colSums(VsLss) * (rowSums(z2[ ,m:n ]))
   }
-  rm( z1,z2 )
+
+  x$z1 <- NULL
+  x$z2 <- NULL
   gc()
   
   # Part 2-5: H10-(5): DVA_INTrexI2
   # OK !
-  z1 <- t(Bm%*%z)
-  for ( tt in 1:G ){
-    m <- 1+( tt-1 )*N; n <-N+(tt-1)*N
+  z1 <- t( x$Bm %*% z )
+  for ( tt in 1:x$G ){
+    m <- 1 + (tt-1) * x$N
+    n <- x$N + (tt-1) * x$N
     z1[ m:n,m:n ] <- 0
   }
-  z2 <- Am*z1
-  for ( r in 1: G ) {
-    m <- 1+( r-1 )*N; n <- N+(r-1)*N
-    ALL[ ,r,5 ] <- colSums(VsLss)*(rowSums(z2[ ,m:n ]))
+  
+  z2 <- x$Am * z1
+  for ( r in 1:x$G ) {
+    m <- 1 + (r-1) * x$N
+    n <- x$N + (r-1) * x$N
+    ALL[ ,r,5 ] <- colSums(VsLss) * (rowSums(z2[ ,m:n ]))
   }
-  rm( z,z1,z2 )
+  
+  x$z <- NULL
+  x$z1 <- NULL
+  x$z2 <- NULL
   gc()
+  
   #  View( ALL[ ,,5 ] )  #  DVA_INTrexI2
   
   # Part 2-6: H10-(6): RDV_FIN
   # OK !
-  z <- array( 0, dim=c( GN,GN ) )
-  for ( tt in 1:G ){
-    m <- 1+( tt-1 )*N; n <-N+(tt-1)*N
-    z[,m:n] <-  Ym[ ,tt ]
+  z <- array( 0, dim=c( x$GN, x$GN ) )
+  for ( tt in 1:x$G ){
+    m <- 1 + (tt-1) * x$N
+    n <- x$N + (tt-1) * x$N
+    z[,m:n] <-  x$Ym[ ,tt ]
   }
-  z1 <- Am*t( Bd%*%z )
-  for ( r in 1: G ) {
-    m <- 1+( r-1 )*N; n <- N+(r-1)*N
-    ALL[ ,r,7 ] <- colSums(VsLss)*(rowSums(z1[ ,m:n ]))
+  
+  z1 <- x$Am * t( x$Bd %*% z )
+
+  for ( r in 1:x$G ) {
+    m <- 1 + (r-1) * x$N
+    n <- x$N + (r-1) * x$N
+    ALL[ ,r,7 ] <- colSums(VsLss) * (rowSums(z1[ ,m:n ]))
   }
-  rm( z1 )
+  
+  x$z1 <- NULL
   gc()
   # View( ALL[ ,,7 ] )  #  RDV_FIN
   
   # Part 2-7 == H10-(7): RDV_FIN2
   # OK !
-  z1 <- Bm%*%z
-  for ( tt in 1:G ){
-    m <- 1+( tt-1 )*N; n <-N+(tt-1)*N
+  z1 <- Bm %*% z
+  for ( tt in 1:x$G ){
+    m <- 1 + (tt-1) * x$N
+    n <- N + (tt-1) * x$N
     z1[ m:n,m:n ] <- 0
   }
-  z2 <- Am*t( z1 )
-  for ( r in 1: G ) {
-    m <- 1+( r-1 )*N; n <- N+(r-1)*N
+  
+  z2 <- Am * t(z1)
+  for ( r in 1:x$G ) {
+    m <- 1 + (r-1) * x$N
+    n <- N + (r-1) * x$N
     ALL[ ,r,8 ] <- colSums(VsLss)*(rowSums(z2[ ,m:n ]))
   }
-  rm( z,z1,z2 )
+  z <- NULL
+  z1 <- NULL
+  z2 <- NULL
   gc()
   
   # View( ALL[ ,,8 ] )  #  RDV_FIN2
   
   # Part 2-8 == H10-(8): RDV_INT
   # OK !
-  z <- array( 0, dim=c( GN,GN ) )
-  for ( tt in 1:G ){
-    m <- 1+( tt-1 )*N; n <-N+(tt-1)*N
-    z[,m:n] <-  Yd[ ,tt ]
+  z <- array( 0, dim=c( x$GN, x$GN ) )
+  for ( tt in 1:x$G ){
+    m <- 1 + (tt-1) * x$N
+    n <- N + (tt-1) * x$N
+    z[,m:n] <-  x$Yd[ ,tt ]
   }
-  z1 <- Am*t( Bm%*%z )
-  for ( r in 1: G ) {
-    m <- 1+( r-1 )*N; n <- N+(r-1)*N
+  
+  z1 <- x$Am * t( x$Bm %*% z )
+  for ( r in 1: x$G ) {
+    m <- 1 + (r-1) * x$N
+    n <- N + (r-1) * x$N
     ALL[ ,r,6 ] <- colSums(VsLss)*(rowSums(z1[ ,m:n ]))
   }
-  rm( z,z1 )
+  
+  z <- NULL
+  z1 <- NULL
   gc()
+
   
   # View( ALL[ ,,6 ] )  #  RDV_INT
   # Part 2-9 == H10-(9): DDC_FIN
   # OK !
-  z <- array( 0, dim=c( GN,GN ) )
-  for ( tt in 1:G ){
-    m <- 1+( tt-1 )*N; n <-N+(tt-1)*N
-    z[m:n,m:n] <-  rowSums( Ym[ m:n, ] )
+  z <- array( 0, dim=c(x$GN, x$GN) )
+  for ( tt in 1:x$G ){
+    m <- 1+ (tt-1) * x$N
+    n <- x$N + (tt-1) * x$N
+    z[m:n,m:n] <-  rowSums( x$Ym[ m:n, ] )
   }
-  z1 <- Am*t( Bm%*%z )
-  for ( r in 1: G ) {
-    m <- 1+( r-1 )*N; n <- N+(r-1)*N
+  
+  z1 <- x$Am * t(x$Bm %*% z)
+  for (r in 1:x$G) {
+    m <- 1 + (r-1) * x$N
+    n <- N + (r-1) * x$N
     ALL[ ,r,13 ] <- colSums(VsLss)*(rowSums(z1[ ,m:n ]))
   }
-  rm( z,z1 )
-  gc()
   
   # View( ALL[ ,,13 ] )  #   DDC_FIN
   
   # Part 2-10 == H10-(10): DDC_INT
   # OK !
-  z <- Am%*%diag(X)
-  for ( r in 1: G ) {
-    m <- 1+( r-1 )*N; n <- N+(r-1)*N
-    ALL[ ,r,14 ] <- colSums(diag(Vc)%*%Bd-VsLss)*(rowSums(z[ ,m:n ]))
+  z <- x$Am %*% diag(X)
+  for (r in 1:x$G) {
+    m <- 1 + (r-1) * x$N
+    n <- x$N + (r-1) * x$N
+    ALL[ ,r,14 ] <- colSums(diag(x$Vc) %*% x$Bd - VsLss) * (rowSums( z[ ,m:n ]) )
   }
-  rm( z )
-  gc()
   
   # View( ALL[ ,,14 ] )  # DDC_INT
   
   # Part 2-11 == H10-(11): MVA_FIN  =[ VrBrs#Ysr ]
   #    H10-(14): OVA_FIN  =[ Sum(VtBts)#rYsr ]
   # OK !
-  VrBrs <-  diag(Vc)%*%Bm
-  YYsr <- array(0, dim=c( GN, GN))
-  for ( r in 1: G ) {
-    m <- 1+( r-1 )*N; n <- N+(r-1)*N
-    YYsr[ ,1:GN ] <- Ym[ ,r ]
-    z <- VrBrs*t( YYsr )
+  VrBrs <-  diag(x$Vc) %*% x$Bm
+  YYsr <- array(0, dim=c(x$GN,x$GN) )
+  for (r in 1:x$G) {
+    m <- 1 + (r-1) * x$N
+    n <- x$N + (r-1) * x$N
+    YYsr[ ,1:x$GN ] <- x$Ym[ ,r ]
+    z <- VrBrs * t( YYsr )
     ALL[ ,r,10 ] <- colSums( z[ m:n, ] ) # MVA_FIN[ ,r ]
     ALL[ ,r,9 ] <- colSums( z[ -c( m:n ), ] ) # OVA_FIN[ ,r ]
   }
@@ -249,16 +288,17 @@ wwz <- function( x, return.totals=FALSE ) {
   # Part 2-13 == H10-(13): MDC  =[ VrBrs#AsrLrrEr* ]
   # ==  H10-(16): ODC  =[ Sum(VtBts)#AsrLrrEr* ]
   # OK !
-  for ( r in 1: G ) {
-    m <- 1+( r-1 )*N; n <- N+(r-1)*N
-    EEr <- array(0, dim=c( GN, GN))
-    EEr[ m:n,1:GN] <- E[ m:n,1 ]
-    z <- VrBrs*t(Am%*%L%*%EEr )
+  for (r in 1:x$G) {
+    m <- 1 + (r-1) * x$N
+    n <- x$N + (r-1) * x$N
+    EEr <- array(0, dim=c(x$GN,x$GN))
+    EEr[ m:n,1:x$GN] <- x$E[ m:n,1 ]
+    z <- VrBrs * t(x$Am %*% x$L %*% EEr )
     ALL[ ,r,16 ] <- colSums( z[ m:n, ] )   # MDC[ ,r ]
     ALL[ ,r,15 ] <-  colSums( z[ -c( m:n ) , ] )   # ODC[ ,r ]
   }
   rm( EEr,z )
-  gc(  )
+  gc()
   
   dimnames( ALL )  <-  list( rownam, regnam, decomp19)
   
@@ -268,7 +308,7 @@ wwz <- function( x, return.totals=FALSE ) {
     out <- ALL
   } else {
     # Part 4 Putting all results in one sheet
-    for ( u in 1: GN ){
+    for ( u in 1: x$GN ){
       if ( u==1 ){
         if( exists( "ALLandTotal" ) ==TRUE )    rm( ALLandTotal )
         ALLandTotal <- rbind( ALL[ u,, ], colSums( ALL[ u,, ] ))
