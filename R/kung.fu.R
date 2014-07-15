c21a10982397f018787d1e144aca19725cc50072
Bastiaan Quast <bquast@gmail.com>
2014-07-15 08:27:34 +0200

    remove attach

--- R/kung.fu.R
+++ R/kung.fu.R
@@ -12,22 +12,14 @@
 kung.fu <- function( x ) {
   
   # Part 1 == loading data A,L,Vc, X, Y, E,ESR, etc.
-  attach( x )
-  
-  # define Vhat
-  Vhat <- diag(GN)
-  diag(Vhat) <- Vc
   
   # decompose
-  out <- Vhat %*% B %*% Exp
+  out <- x$Vhat %*% x$B %*% x$Exp
   
   # add row and column names
   out <- as.data.frame(out)
-  names(out) <- rownam
-  row.names(out) <- rownam
-  
-  # detach object
-  detach(x)
+  names(out) <- x$rownam
+  row.names(out) <- x$rownam
   
   # return result
   return( out )
