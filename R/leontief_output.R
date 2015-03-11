85e819a630821569417bd0f5251a81b9e270d538
Bastiaan Quast <bquast@gmail.com>
2015-03-11 12:58:01 +0100

    remove leontief_output to separate file

--- R/leontief_output.R
+++ R/leontief_output.R
@@ -1,10 +1,10 @@
-#' Leontief Decomposition
+#' Leontief Output Decomposition
 #' 
-#' This function runs the Leontief decomposition.
+#' This function runs the Leontief decomposition on output
 #' 
 #' @param x ane object of class decompr
-#' @param tidy transform the output data into a tidy data set or not, default it TRUE.
 #' @return a data frame containing the square matrix and labelled column and rows
+#' @param tidy transform the output data into a tidy data set or not, default it TRUE.
 #' @author Bastiaan Quast
 #' @references Wang, Zhi, Shang-Jin Wei, and Kunfu Zhu.
 #' Quantifying international production sharing at the bilateral and sector levels. 
@@ -22,41 +22,24 @@
 #'                                       out        )
 #' 
 #' # run the Leontief decomposition on the decompr object
-#' leontief(decompr_object, tidy=FALSE )
+#' leontief_output(decompr_object)
 
 
-leontief <- function( x, tidy=TRUE ) {
+leontief_output <- function( x, tidy=TRUE ) {
   
   # Part 1 == loading data A,L,Vc, X, Y, E,ESR, etc.
   
   # decompose
-  out <- x$Vhat %*% x$B %*% x$Exp
+  out <- x$Vhat %*% x$B %*% diag(x$X)
   
-  
-  if (tidy == TRUE) {
-    
-    out <- as.vector(t(out))
-    out <- data.frame( rep(x$k,                  each = x$GN*x$N ),
-                       rep(x$i, times = x$G,     each = x$GN),
-                       rep(x$k, times = x$GN,    each = x$N),
-                       rep(x$i, times = x$GN*x$G ),
-                       out)
-    names(out) <- c("Source_Country", "Source_Industry", "Using_Country", "Using_Industry", "FVAX")
-    
-  } else {
-    
-    # add row and column names
-    out <- as.data.frame(out)
-    names(out) <- x$rownam
-    row.names(out) <- x$rownam
-    
-  }
-  
-  
+  # add row and column names
+  out <- as.data.frame(out)
+  names(out) <- x$rownam
+  row.names(out) <- x$rownam
   
   # create attributes
-  attr(out, "k")      <- x$k
-  attr(out, "i")      <- x$i
+  attr(out, "k")      <- x$i
+  attr(out, "i")      <- x$k
   # attr(out, "rownam") <- x$rownam
   
   # return result
