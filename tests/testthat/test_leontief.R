a597950a75fe70213afa16e13a243d1469c2f171
Bastiaan Quast <bquast@gmail.com>
2015-03-14 17:48:03 +0100

    spacing for legibility

--- tests/testthat/test_leontief.R
+++ tests/testthat/test_leontief.R
@@ -16,10 +16,10 @@ l <- decomp(inter,
 context("output format")
 
 test_that("output size matches", {
-  expect_equal(length(l), 5 )
-  expect_equal(dim(l)[1], 81 )
+  expect_equal( length(l), 5 )
+  expect_equal( dim(l)[1], 81 )
 })
 
 test_that("output format matches", {
-  expect_match(typeof(l[,5]), "double")
+  expect_match( typeof(l[,5]), "double" )
 })
