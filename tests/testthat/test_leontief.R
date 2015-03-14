bb0b194717b7ca3e0d932223e7e98da0399da08d
Bastiaan Quast <bquast@gmail.com>
2015-03-14 17:20:26 +0100

    more tests

--- tests/testthat/test_leontief.R
+++ tests/testthat/test_leontief.R
@@ -15,6 +15,11 @@ l <- decomp(inter,
 # define context
 context("output format")
 
-test_that("Output format matches", {
+test_that("output size matches", {
   expect_equal(length(l), 5 )
+  expect_equal(dim(l)[1], 81 )
+})
+
+test_that("output format matches", {
+  expect_match(typeof(l[,5]), "double")
 })
