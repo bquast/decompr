7de9caf3092682427a63ed0907f8e6bbd2a2f010
Bastiaan Quast <bquast@gmail.com>
2015-04-23 12:10:51 +0200

    test output content, rely on defaults

--- tests/testthat/test_leontief.R
+++ tests/testthat/test_leontief.R
@@ -5,16 +5,18 @@ library(decompr)
 data(leather)
 
 # leontief decomposition
+# n.b. using default method (Leontief)
+# with default post-multiplication (exports)
 l <- decomp(inter,
             final,
             countries,
             industries,
-            out,
-            method = "leontief")
+            out)
 
 # define context
 context("output format")
 
+# test output format (i.e. structure not numbers)
 test_that("output size matches", {
   expect_equal( length(l), 5 )
   expect_equal( dim(l)[1], 81 )
@@ -23,3 +25,14 @@ test_that("output size matches", {
 test_that("output format matches", {
   expect_match( typeof(l[,5]), "double" )
 })
+
+# define context
+context("output content")
+
+# test output content (i.e. numbers)
+test_that("output matches", {
+  expect_equal( l[1, 5],  28.52278, tolerance = .002 )
+  expect_equal( l[81, 5], 34.74381, tolerance = .002 )
+})
+
+
