1b0dca6a079cbf781363f069f2175ae60d51f3fe
Bastiaan Quast <bquast@gmail.com>
2015-06-01 10:10:51 +0200

    move since now one function

--- tests/testthat/test_leontief.R
+++ tests/testthat/test_leontief.R
@@ -1,5 +1,5 @@
-# load the package
-library(decompr)
+# define context
+context("leontief")
 
 # load test data
 data(leather)
@@ -13,9 +13,6 @@ l <- decomp(inter,
             industries,
             out)
 
-# define context
-context("output format")
-
 # test output format (i.e. structure not numbers)
 test_that("output size matches", {
   expect_equal( length(l), 5 )
@@ -26,11 +23,35 @@ test_that("output format matches", {
   expect_match( typeof(l[,5]), "double" )
 })
 
-# define context
-context("output content")
-
 # test output content (i.e. numbers)
 test_that("output matches", {
   expect_equal( l[1, 5],  28.52278, tolerance = .002 )
   expect_equal( l[81, 5], 34.74381, tolerance = .002 )
+})
+
+
+context("leontief-output")
+
+# leontief decomposition
+lo <- decomp(inter,
+             final,
+             countries,
+             industries,
+             out,
+             method = "leontief",
+             post = "output")
+
+test_that("output size matches", {
+  expect_equal( length(lo), 5 )
+  expect_equal( dim(lo)[1], 81 )
+})
+
+test_that("output format matches", {
+  expect_match(typeof( lo[,5]), "double" )
+})
+
+# test output content (i.e. numbers)
+test_that("output matches", {
+  expect_equal( lo[1, 5],  66.75361799, tolerance = .002 )
+  expect_equal( lo[81, 5], 96.78316785, tolerance = .002 )
 })
