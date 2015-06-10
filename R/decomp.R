8641f28f134cd7cfdb3a954c1faa62af6992beb8
Bastiaan Quast <bquast@gmail.com>
2015-06-10 13:58:44 +0200

    nothing much, align warning message

--- R/decomp.R
+++ R/decomp.R
@@ -1,9 +1,9 @@
 #' Interface function for decompositions
-#' 
+#'
 #' This function runs the decomposition.
 #' NOTE: the default method is now "leontief", please specify method="wwz" explicitly for Wang-Wei-Zhu.
 #' See http://qua.st/decompr/decompr-v2/ for more information.
-#' 
+#'
 #' @param x the intermediate demand table, it has dimensions GN x GN (G = no. of country, N = no. of industries),
 #'  excluding the first row and the first column which contains the country names,
 #'  and the second row and second column which contain the industry names for each country.
@@ -24,25 +24,25 @@
 #' Furthermore, the input object have a different structure, see the information below for details.
 #' @author Bastiaan Quast
 #' @references {Timmer, Marcel P. (ed) (2012), "The World Input-Output Database (WIOD): Contents Sources and Methods", WIOD Working Paper Number 10, downloadable at http://www.wiod.org/publications/papers/wiod10.pdf }
-#' 
+#'
 #' {Wang, Zhi, Shang-Jin Wei, and Kunfu Zhu. Quantifying international production sharing at the bilateral and sector levels. No. w19677. National Bureau of Economic Research, 2013.}
 #' @export
 #' @examples
 #' # load leather example data
 #' data(leather)
-#' 
+#'
 #' # explore the data set
 #' ls()
-#' 
+#'
 #' # explore each of the objects
 #' inter
 #' final
 #' countries
 #' industries
 #' out
-#' 
+#'
 #' # use the direct approach
-#' 
+#'
 #' # run the Leontief decomposition
 #' decomp(inter,
 #'        final,
@@ -50,7 +50,7 @@
 #'        industries,
 #'        out,
 #'        method = "leontief")
-#' 
+#'
 #' # run the WWZ decomposition
 #' decomp(inter,
 #'        final,
@@ -61,23 +61,23 @@
 
 
 
-decomp <- function( x, y, k, i, o,  method=c("leontief", "wwz" ), ... ) {  
-  
+decomp <- function( x, y, k, i, o,  method=c("leontief", "wwz" ), ... ) {
+
   if ( missing(method) ) {
     message('No method specified, the default method in version 2 of decompr has been changed to Leontief.
 
-            In order to use the Wang-Wei-Zhu (cf. decompr v.1), please specify this explicitly using: method="wwz"')
+  In order to use the Wang-Wei-Zhu (cf. decompr v.1), please specify this explicitly using: method="wwz"')
   }
-  
+
   method <- match.arg(method)
-  
+
   if ( missing(k) | missing(i) | missing(o) ) {
     warning('argument k, i, or o is missing, switching to the old "load_tables" function, which is DEPRECATED! Please see "help(decomp) and "http://qua.st/decompr/decompr-v2/" for more information on this.')
     decompr_obj <- load_tables(x, y)
   }  else {
     decompr_obj <- load_tables_vectors(x, y, k, i, o)
   }
-  
+
   if ( method == "leontief" ) {
     out <- leontief( decompr_obj, ... )
   } else if (method == "wwz" ) {
@@ -85,7 +85,7 @@ decomp <- function( x, y, k, i, o,  method=c("leontief", "wwz" ), ... ) {
   } else {
     stop('not a valid method')
   }
-  
+
   return(out)
-  
+
 }
