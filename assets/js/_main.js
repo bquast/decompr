7b6a91e6c33ea53a04b3ddd82094f38caa9cfeaa
Bastiaan Quast <bquast@gmail.com>
2016-09-20 11:09:14 +0200

    update to minimal mistakes 3.4 setup config.yml

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,54 +1,78 @@
-/*! Responsive Menu */
-// http://tympanus.net/codrops/2013/05/08/responsive-retina-ready-menu/
-//  The function to change the class
-var changeClass = function (r,className1,className2) {
-  var regex = new RegExp("(?:^|\\s+)" + className1 + "(?:\\s+|$)");
-  if( regex.test(r.className) ) {
-    r.className = r.className.replace(regex,' '+className2+' ');
-    }
-    else{
-    r.className = r.className.replace(new RegExp("(?:^|\\s+)" + className2 + "(?:\\s+|$)"),' '+className1+' ');
-    }
-    return r.className;
-};  
-//  Creating our button in JS for smaller screens
-var menuElements = document.getElementById('site-nav');
-menuElements.insertAdjacentHTML('afterBegin','<button type="button" role="button" id="menutoggle" class="navtoogle navicon-lines-button x" aria-hidden="true"><span class="navicon-lines"></span>menu</button>');
+/* ==========================================================================
+   jQuery plugin settings and other scripts
+   ========================================================================== */
 
-//  Toggle the class on click to show / hide the menu
-document.getElementById('menutoggle').onclick = function() {
-  changeClass(this, 'navtoogle active', 'navtoogle');
-};
-// http://tympanus.net/codrops/2013/05/08/responsive-retina-ready-menu/comment-page-2/#comment-438918
-document.onclick = function(e) {
-  var mobileButton = document.getElementById('menutoggle'),
-    buttonStyle =  mobileButton.currentStyle ? mobileButton.currentStyle.display : getComputedStyle(mobileButton, null).display;
+$(document).ready(function(){
 
-  if(buttonStyle === 'block' && e.target !== mobileButton && new RegExp(' ' + 'active' + ' ').test(' ' + mobileButton.className + ' ')) {
-    changeClass(mobileButton, 'navtoogle active', 'navtoogle');
-  }
-};
+  // Sticky footer
+  var bumpIt = function() {
+      $('body').css('margin-bottom', $('.page__footer').outerHeight(true));
+    },
+    didResize = false;
 
-/*! Plugin options and other jQuery stuff */
+  bumpIt();
 
-// FitVids options
-$(function() {
-	$("article").fitVids();
-});
-
-// Table of Contents toggle
-$(function() {
-  $(".toc h3").click(function () {
-    $("#drawer").toggleClass("js-hidden");
+  $(window).resize(function() {
+    didResize = true;
   });
-});
+  setInterval(function() {
+    if(didResize) {
+      didResize = false;
+      bumpIt();
+    }
+  }, 250);
 
-// Add lightbox class to all image links
-$("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
+  // FitVids init
+  $("#main").fitVids();
 
-// Magnific-Popup options
-$(document).ready(function() {
-  $('.image-popup').magnificPopup({
+  // init sticky sidebar
+  $(".sticky").Stickyfill();
+
+  var stickySideBar = function(){
+    var show = $(".author__urls-wrapper button").length === 0 ? $(window).width() > 1024 : !$(".author__urls-wrapper button").is(":visible");
+    // console.log("has button: " + $(".author__urls-wrapper button").length === 0);
+    // console.log("Window Width: " + windowWidth);
+    // console.log("show: " + show);
+    //old code was if($(window).width() > 1024)
+    if (show) {
+      // fix
+      Stickyfill.rebuild();
+      Stickyfill.init();
+      $(".author__urls").show();
+    } else {
+      // unfix
+      Stickyfill.stop();
+      $(".author__urls").hide();
+    }
+  };
+
+  stickySideBar();
+
+  $(window).resize(function(){
+    stickySideBar();
+  });
+
+  // Follow menu drop down
+
+  $(".author__urls-wrapper button").on("click", function() {
+    $(".author__urls").fadeToggle("fast", function() {});
+    $(".author__urls-wrapper button").toggleClass("open");
+  });
+
+  // init smooth scroll
+  $("a").smoothScroll({offset: -20});
+
+  // add lightbox class to all image links
+  $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
+
+  // Magnific-Popup options
+  $(".image-popup").magnificPopup({
+    // disableOn: function() {
+    //   if( $(window).width() < 500 ) {
+    //     return false;
+    //   }
+    //   return true;
+    // },
     type: 'image',
     tLoading: 'Loading image #%curr%...',
     gallery: {
@@ -59,9 +83,18 @@ $(document).ready(function() {
     image: {
       tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
     },
-    removalDelay: 300, // Delay in milliseconds before popup is removed
-    // Class that is added to body when popup is open. 
+    removalDelay: 500, // Delay in milliseconds before popup is removed
+    // Class that is added to body when popup is open.
     // make it unique to apply your CSS animations just to this exact popup
-    mainClass: 'mfp-fade'
+    mainClass: 'mfp-zoom-in',
+    callbacks: {
+      beforeOpen: function() {
+        // just a hack that adds mfp-anim class to markup
+        this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+      }
+    },
+    closeOnContentClick: true,
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
+
 });
